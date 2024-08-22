8898843f6f02cca51b5c27968f9522d44b119e19
Leeza Dinh <dinh.leeza058@gmail.com>
2024-08-22 13:08:22 -0400

    jsadded

--- script.js
+++ script.js
@@ -1,1 +1,82 @@
 "use strict";
+const productList = document.querySelector(".product-list");
+
+const products = [
+  {
+    name: "Keyboard",
+    price: 100,
+    description: "lorem",
+    category: "digital",
+  },
+  //   change the info for the rest of the objects
+  {
+    name: "Keyboard",
+    price: 100,
+    description: "lorem",
+    category: "digital",
+  },
+  {
+    name: "Keyboard",
+    price: 100,
+    description: "lorem",
+    category: "digital",
+  },
+  {
+    name: "Keyboard",
+    price: 100,
+    description: "lorem",
+    category: "digital",
+  },
+  {
+    name: "Keyboard",
+    price: 100,
+    description: "lorem",
+    category: "digital",
+  },
+  {
+    name: "Keyboard",
+    price: 100,
+    description: "lorem",
+    category: "digital",
+  },
+  {
+    name: "Keyboard",
+    price: 100,
+    description: "lorem",
+    category: "digital",
+  },
+  {
+    name: "Keyboard",
+    price: 100,
+    description: "lorem",
+    category: "digital",
+  },
+  {
+    name: "Keyboard",
+    price: 100,
+    description: "lorem",
+    category: "digital",
+  },
+  {
+    name: "Keyboard",
+    price: 100,
+    description: "lorem",
+    category: "digital",
+  },
+];
+
+// make an unordered list inside of product list
+// then make list items inside of the UL
+
+// this is the list item i need to make in js
+// <div class="1">
+//     <!-- <img> -->
+//     <button>Add to Cart</button>
+//     <h2></h2>
+//     <p></p>
+//     <p></p>
+//   </div>
+
+// need to make a forEachloop for a specified amount of time
+
+// array [products{}]
